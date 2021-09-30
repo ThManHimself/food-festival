0f69a62136a3ea75a1965b44e90d41f5f81b22d2
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-30 00:12:37 -0500

    minor changes

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ const APP_PREFIX = 'FoodEvent-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./events.html",
-    "./tickets.html",
-    "./schedule.html",
+    "./public/index.html",
+    "./public/events.html",
+    "./public/tickets.html",
+    "./public/schedule.html",
     "./assets/css/style.css",
     "./assets/css/bootstrap.css",
     "./assets/css/tickets.css",
@@ -16,6 +16,37 @@ const FILES_TO_CACHE = [
 ];
 
 
+// Cache resources
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE);
+        })
+        )
+    })
+    
+// Delete outdated caches
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeepList = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+    
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+            
 // Respond with cache resources
 self.addEventListener('fetch', function(e) {
     console.log('fetch request : ' + e.request.url)
@@ -34,35 +65,3 @@ self.addEventListener('fetch', function(e) {
         })
     )
 })
-
-// Cache resources
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function(e) {
-    e.waitUntil(
-        caches.keys().then(function(keyList) {
-            let cacheKeepList = keyList.filter(function(key) {
-                return key.indexOf(APP_PREFIX);
-            })
-            cacheKeepList.push(CACHE_NAME);
-
-            return Promise.all(
-                keyList.map(function(key, i) {
-                    if (cacheKeepList.indexOf(key) === -1) {
-                        console.log('deleting cache : ' + keyList[i]);
-                        return caches.delete(keyList[i]);
-                    }
-                })
-            );
-        })
-    );
-});
-
